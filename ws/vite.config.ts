628672f272bf2d2f03d520a24df0d985d7ed506f
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-09-10 16:11:05 +0900

    modify base path

--- ws/vite.config.ts
+++ ws/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './react-vite-actions/',
+  base: '/react-vite-actions/',
   plugins: [react()],
   resolve: {
     alias: {
