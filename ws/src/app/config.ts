d595f1811a88a37c6bd02558a50ce58bffe1d4f4
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-09-10 23:04:22 +0900

    make source code minimal

--- ws/src/app/config.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-export const width = 1600;
-export const height = 900;
-export const bin_size = 10;
-export const ITEMBUFFER_COLOR: THREE.ColorRepresentation = 'skyblue';
-export const TEMPORARYSAVE_COLOR: THREE.ColorRepresentation = 'greenyellow';
